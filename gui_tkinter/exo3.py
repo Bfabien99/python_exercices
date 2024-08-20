c0c3e64599839e2817c1e32970384cf8775066d0
Brou kouadio Stéphane-Fabien <fabienbrou99@gmail.com>
2024-08-20 10:58:40 +0000

    ajout de pions au clic

--- gui_tkinter/exo3.py
+++ gui_tkinter/exo3.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 
 # cercle pour les différents pions
 def cercle(x, y, r):
@@ -8,18 +9,25 @@ def cercle(x, y, r):
 def carre(x1, y1, x2,color="blue"):
     can.create_rectangle(x1, y1, x2, y1+20, outline="black", fill=color)
 
+def draw_circle():
+    draw_pan()
+    for i in range(0, 181, 20):
+        for j in range(0, 181, 20):
+            f = random.randint(0,1)
+            cercle(j+10, i+10, 5) if f else None
+
 def draw_pan():
-    x1 = 0
-    x2 = 20
-    y1 = 0
+    # x1 = 0
+    # x2 = 20
+    # y1 = 0
     b = True
     for i in range(0, 181, 20):
         for j in range(0, 181, 20):
             if b:
                 carre(j, i, j+20)
             else:
-                carre(j, i, j+20, "red")
-            b = not b 
+                carre(j, i, j+20, "ivory")
+            b = not b
                 
 fen = Tk()
 can = Canvas(fen, width =200, height =200, bg ='ivory')
@@ -27,5 +35,8 @@ can.pack(side =TOP, padx =5, pady =5)
 
 btn_square = Button(fen, text="Damier", command=draw_pan)
 btn_square.pack(side=LEFT)
+
+btn_circle = Button(fen, text="Pions", command=draw_circle)
+btn_circle.pack(side=RIGHT)
 
 fen.mainloop()
