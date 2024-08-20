44fdd551ed80d313286d984a1ae1582a2b953348
Brou kouadio Stéphane-Fabien <fabienbrou99@gmail.com>
2024-08-20 12:01:37 +0000

    application de dessin petit

--- gui_tkinter/exo4.py
+++ gui_tkinter/exo4.py
@@ -3,15 +3,16 @@ from tkinter import *
 
 def cercle(x, y, r):
     "tracé d'un cercle de centre (x,y) et de rayon r"
-    cadre.create_oval(x-r, y-r, x+r, y+r, outline="black", fill="white")
+    cadre.create_oval(x-r, y-r, x+r, y+r, fill="white")
     
 def pointeur(event):
     chaine.configure(text = "Clic détecté en X =" + str(event.x) + ", Y =" + str(event.y))
     cercle(event.x, event.y, 1)
     
 fen = Tk()
-cadre = Canvas(fen, width =300, height =300, bg="black")
-cadre.bind("<Button-1>", pointeur)
+cadre = Canvas(fen, width =1000, height =1000, bg="black")
+cadre.bind("<Button-1>", pointeur) # au clic
+cadre.bind("<B1-Motion>", pointeur) # clic enfoncé
 cadre.pack()
 chaine = Label(fen)
 chaine.pack()
