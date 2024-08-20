859a7293b9863ab646c5562caed74a94aadf8cbd
Brou kouadio Stéphane-Fabien <fabienbrou99@gmail.com>
2024-08-20 10:50:06 +0000

    tracé de carré sur un bloc

---
+++ gui_tkinter/exo3.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+# cercle pour les différents pions
+def cercle(x, y, r):
+    "tracé d'un cercle de centre (x,y) et de rayon r"
+    can.create_oval(x-r, y-r, x+r, y+r, outline="black", fill="red")
+
+def carre(x1, y1, x2,color="blue"):
+    can.create_rectangle(x1, y1, x2, y1+20, outline="black", fill=color)
+
+def draw_pan():
+    x1 = 0
+    x2 = 20
+    y1 = 0
+    b = True
+    for i in range(0, 181, 20):
+        for j in range(0, 181, 20):
+            if b:
+                carre(j, i, j+20)
+            else:
+                carre(j, i, j+20, "red")
+            b = not b 
+                
+fen = Tk()
+can = Canvas(fen, width =200, height =200, bg ='ivory')
+can.pack(side =TOP, padx =5, pady =5)
+
+btn_square = Button(fen, text="Damier", command=draw_pan)
+btn_square.pack(side=LEFT)
+
+fen.mainloop()
